a11fe88cdd353d80b3c3f48bd411ae46e8a1ca91
Jewel Osafile <osafilejewel@gmail.com>
2022-09-28 17:10:43 +0100

    addd expense

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Expense;
 use Illuminate\Http\Request;
 
 class ExpenseController extends Controller
 {
-    
+    public function store(Request $request){
+        $expenseFields = $request->validate([
+            'date' => 'required',
+            'merchant' => 'required',
+            'total' => 'required',
+            'comment' => 'required',
+        ]);
+        // return $request;
+            Expense::create($expenseFields);
+        return redirect('/')->with('status', 'Expense added Successfully!');        
+    }   
 }
